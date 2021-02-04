16d2bc8bf267fbd54a28770d7ea3cf323a9a0ab2
Yunjun Wang <yunjun.wangirl@gmail.com>
2021-02-04 22:35:14 +0000

    Update yunjun_exercise.js

--- 2-JS-basics/starter/yunjun_exercise.js
+++ 2-JS-basics/starter/yunjun_exercise.js
@@ -230,4 +230,119 @@
 
     }
 
+
+    /*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+var billsJohn = {
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        var bill;
+        var percentage;
+        var tip = 0;
+        this.tips = [];
+        this.payments = [];
+        
+        for(var i = 0; i < this.bills.length; i++) {
+            bill = this.bills[i];
+            switch(true) {
+                case bill < 50:
+                    percentage = .2;
+                    break;
+                case bill >= 50 && bill < 200:
+                    percentage = .15;
+                    break;
+                case bill >= 200:
+                    percentage = .1;
+                    break;
+            }
+
+            tip = bill * percentage;
+            this.tips[i] = tip;
+            this.payments[i] = bill + tip;
+        }
+        
+    }
+};
+
+var billsMark = {
+    bills: [77, 375, 110, 45],
+    calcTips: function() {
+        var bill;
+        var percentage;
+        var tip = 0;
+        this.tips = [];
+        this.payments = [];
+        
+        for(var i = 0; i < this.bills.length; i++) {
+            bill = this.bills[i];
+            switch(true) {
+                case bill < 100:
+                    percentage = .2;
+                    break;
+                case bill >= 100 && bill < 300:
+                    percentage = .1;
+                    break;
+                case bill >= 300:
+                    percentage = .25;
+                    break;
+            }
+
+            tip = bill * percentage;
+            this.tips[i] = tip;
+            this.payments[i] = bill + tip;
+        }
+        
+    }
+};
+
+function calcAverageTip (tips) {
+    var sum = 0;
+    var size = tips.length;
+
+    for(var i = 0; i < size; i++) {
+        sum += tips[i];
+    }
+
+    return sum / size;
+}
+billsJohn.calcTips();
+billsMark.calcTips();
+john.ave = calcAverageTip(billsJohn.tips);
+mark.ave = calcAverageTip(billsMark.tips);
+
+if(john.ave > mark.ave) {
+    console.log('John family paid the highest tips on average: ' + john.ave);
+} else if( mark.ave > john.ave) {
+    console.log('Mark family paid the highest tips on average: ' + mark.ave);
+} else {
+    console.log('John and Mark families paid the same tips on average: ' + john.ave);
+}
+
+
 })();
